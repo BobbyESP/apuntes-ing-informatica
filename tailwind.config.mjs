eac241801f5f3c3625ac33c9e9bbb8525ab6ca22
Gabriel Fontán <gabilessto@gmail.com>
2025-03-27 17:48:58 +0100

    Lower contrast

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 import starlightPlugin from '@astrojs/starlight-tailwind';
 
 // Generated color palettes
-const accent = { 200: '#b1d4ac', 600: '#065b00', 900: '#093e05', 950: '#0e2c0b' };
+const accent = { 200: '#b1d4ac', 600: '#0b7c00', 900: '#093e05', 950: '#0e2c0b' };
 const gray = { 100: '#f6f6f5', 200: '#edeeec', 300: '#c1c2c0', 400: '#8a8c88', 500: '#575955', 700: '#373936', 800: '#262724', 900: '#181817' };
 
 /** @type {import('tailwindcss').Config} */
